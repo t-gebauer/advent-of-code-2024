7354792fd27a0cad9527f556267bf0af7c331805
t-gebauer <t-gebauer@users.noreply.github.com>
2024-12-18 10:43:38 +0100

    day 11 part 2

--- day11.ml
+++ day11.ml
@@ -32,11 +32,38 @@ let%expect_test _ =
   blink_multiple 6 example_stones2 |> List.length |> print_int;
   [%expect {| 22 |}]
 
+let blink_count_sum times stones =
+  let h = Hashtbl.create 10000 in
+  let rec blink_count times stone =
+    try Hashtbl.find h (times, stone)
+    with Not_found ->
+      let c =
+        if times = 0 then 1
+        else if stone = 0 then blink_count (times - 1) 1
+        else if is_even (number_of_digits stone) then
+          split_stone stone |> List.map (blink_count (times - 1)) |> Lib.sum
+        else blink_count (times - 1) (stone * 2024)
+      in
+      Hashtbl.add h (times, stone) c;
+      c
+  in
+  List.map (blink_count times) stones |> Lib.sum
+
+let%expect_test _ =
+  blink_count_sum 6 example_stones2 |> print_int;
+  [%expect {| 22 |}]
+
+let%expect_test _ =
+  blink_count_sum 25 example_stones2 |> print_int;
+  [%expect {| 55312 |}]
+
 let part1 lines =
   let stones = Lib.extract_numbers (List.hd lines) in
   blink_multiple 25 stones |> List.length |> string_of_int
 
-let part2 _lines = "TODO"
+let part2 lines =
+  let stones = Lib.extract_numbers (List.hd lines) in
+  blink_count_sum 75 stones |> string_of_int
 
 let example = [ "125 17" ]
 
@@ -46,4 +73,4 @@ let%expect_test _ =
 
 let%expect_test _ =
   print_string (part2 example);
-  [%expect {| TODO |}]
+  [%expect {| 65601038650482 |}]
