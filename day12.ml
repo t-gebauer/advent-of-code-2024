1af94751bd91558d9d9bc0ccb88777ab9da4d483
t-gebauer <t-gebauer@users.noreply.github.com>
2024-12-20 11:16:27 +0100

    day 12 generalize and combine

--- day12.ml
+++ day12.ml
@@ -1,99 +1,70 @@
 module PMap = Map.Make (Lib.IntPair)
+module PSet = Set.Make (Lib.IntPair)
 module V = Vec2i
 
-type region = { area : int; perimiter : int }
-
 let find_regions map =
-  let visited = Grid.(create map.width map.height (fun _ -> '.')) in
-  let rec expand_region id pos : region =
-    Grid.set pos '+' visited;
+  let rec expand_region char pos region =
+    let region = PSet.add pos region in
     [ V.up; V.down; V.left; V.right ]
     |> List.map (V.add pos)
-    |> List.map (fun p ->
-           let c = try Grid.get p map with Grid.Out_of_bounds -> ' ' in
-           if c <> id then { area = 0; perimiter = 1 }
-           else
-             let vis = Grid.get p visited in
-             if vis = '+' then { area = 0; perimiter = 0 }
-             else expand_region id p)
     |> List.fold_left
-         (fun acc reg ->
-           {
-             area = acc.area + reg.area;
-             perimiter = acc.perimiter + reg.perimiter;
-           })
-         { area = 1; perimiter = 0 }
+         (fun reg p ->
+           if Grid.get_opt p map = Some char && not (PSet.mem p reg) then
+             expand_region char p reg
+           else reg)
+         region
   in
+  let visited = Grid.(create map.width map.height (fun _ -> '.')) in
   Grid.flat_mapi
-    (fun p c ->
-      if not (Grid.get p visited = '+') then Some (expand_region c p) else None)
+    (fun pos char ->
+      match Grid.get pos visited with
+      | '.' ->
+          let region = expand_region char pos PSet.empty in
+          PSet.iter (fun p -> Grid.set p '+' visited) region;
+          Some region
+      | _ -> None)
     map
   |> List.filter_map (fun x -> x)
 
-let price_of_region region = region.area * region.perimiter
+let count_perimiter region =
+  let count_borders pos =
+    let has_neighbor dir = PSet.mem (V.add pos dir) region in
+    [ V.up; V.down; V.left; V.right ]
+    |> List.map (fun p -> if has_neighbor p then 0 else 1)
+    |> Lib.sum
+  in
+  PSet.to_list region |> List.map count_borders |> Lib.sum
 
 let part1 lines =
   let map = Grid.create_from_lines lines in
-  find_regions map |> List.map price_of_region |> Lib.sum |> string_of_int
+  find_regions map
+  |> List.map (fun reg ->
+         let area = PSet.cardinal reg in
+         let perimiter = count_perimiter reg in
+         area * perimiter)
+  |> Lib.sum |> string_of_int
 
-type region_mut = { mutable area : int; mutable sides : int }
-
-let find_regions2 map =
-  let regions = Hashtbl.create 100 in
-  let rec expand_region char pos region =
-    Hashtbl.add regions pos region;
-    region.area <- region.area + 1;
-    [ V.up; V.down; V.left; V.right ]
-    |> List.map (V.add pos)
-    |> List.iter (fun p ->
-           try
-             if
-               Grid.get p map = char
-               && Option.is_none (Hashtbl.find_opt regions p)
-             then expand_region char p region
-           with Grid.Out_of_bounds -> ())
+let count_sides region =
+  let count_corners pos =
+    let has_neighbor dir = PSet.mem (V.add pos dir) region in
+    [ V.up_left; V.up_right; V.down_left; V.down_right ]
+    |> List.map (fun d ->
+           let x, y = d in
+           let nx = has_neighbor (x, 0) in
+           let ny = has_neighbor (0, y) in
+           let center = has_neighbor d in
+           if ((not nx) && not ny) || (nx && ny && not center) then 1 else 0)
+    |> Lib.sum
   in
-  let count_corners char pos region =
-    let get_neighbor dir =
-      try Grid.get (V.add dir pos) map with Grid.Out_of_bounds -> ' '
-    in
-    region.sides <-
-      region.sides
-      + ([ V.up_left; V.up_right; V.down_left; V.down_right ]
-        |> List.map (fun d ->
-               let x, y = d in
-               let nx = get_neighbor (x, 0) in
-               let ny = get_neighbor (0, y) in
-               let center = get_neighbor d in
-               if
-                 (nx <> char && ny <> char)
-                 || (nx = char && ny = char && center <> char)
-               then 1
-               else 0)
-        |> Lib.sum)
-  in
-  let reg =
-    Grid.flat_mapi
-      (fun pos char ->
-        match Hashtbl.find_opt regions pos with
-        | None ->
-            let region = { area = 0; sides = 0 } in
-            expand_region char pos region;
-            Some region
-        | Some _ -> None)
-      map
-  in
-  Grid.iteri
-    (fun pos char -> count_corners char pos (Hashtbl.find regions pos))
-    map;
-  reg |> List.filter_map (fun x -> x)
-
-let bulk_diskcounted_price region = region.area * region.sides
+  PSet.to_list region |> List.map count_corners |> Lib.sum
 
 let part2 lines =
   let map = Grid.create_from_lines lines in
-  find_regions2 map
-  |> List.map bulk_diskcounted_price
+  find_regions map
+  |> List.map (fun reg ->
+         let area = PSet.cardinal reg in
+         let sides = count_sides reg in
+         area * sides)
   |> Lib.sum |> string_of_int
 
 let example1 = Lib.parse_lines {|
